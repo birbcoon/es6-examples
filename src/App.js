7d93f58f3aaa0d484cdc4a853b2d2f9f0a7356bf
Andy <russellandy98@gmail.com>
2019-08-22 16:15:15 -0400

    Setup console.log for examples and did example 1

--- src/App.js
+++ src/App.js
@@ -23,4 +23,74 @@ function App() {
   );
 }
 
+const movies = [
+  {name: "John Wick 3", year: 2019, rating: "5 stars"},
+  {name: "Youngblood", year: 1986, rating: "1 stars"},
+  {name: "The Night Before", year: 1988, rating: "3 stars"},
+  {name: "Bill & Ted's Excellent Adventure", year: 1989, rating: "5 stars"},
+  {name: "The Matrix", year: 1999, rating: "4 stars"},
+  {name: "John Wick", year: 2014, rating: "5 stars"}
+];
+//Example 1 Default Parameters
+console.log("Example 1: Default Parameters");
+const defaultClass = {name: "Full Stack", teacher: {first: "Zhiguang", last: "Xu"}, number: "CS4840"};
+function defaultExample(p = defaultClass) {
+  console.log(`Dr. ${p.teacher.last} teaches ${p.name}`)
+}
+console.log("Default with no parameters provided:");
+defaultExample();
+console.log("When parameters are passed:")
+defaultExample({name: "Big Data", teacher:{last: "Mihail"}});
+console.log('-------------');
+
+//Example 2 Arrow functions
+console.log("Example 2: Arrow functions");
+
+console.log('-------------');
+
+//Example 3 Destrunction Assignment
+console.log("Example 3: Destruction Assignment");
+
+console.log('-------------');
+
+//Example 4 Spread Operator
+console.log("Example 4: Spread Operator");
+
+console.log('-------------');
+
+//Example 5 Promises
+console.log("Example 5: Promises");
+
+console.log('-------------');
+
+//Example 6 Object.assign
+console.log("Example 6: Object.Assign");
+
+console.log('-------------');
+
+//Example 7 Spread Operator
+console.log("Example 7: Spread Operator");
+
+console.log('-------------');
+
+//Example 8 Array.filer
+console.log("Example 8: Array.filler");
+
+console.log('-------------');
+
+//Example 9 Array.map
+console.log("Example 9: Array.map");
+
+console.log('-------------');
+
+//Example 10 Array.slice
+console.log("Example 10: Array.slice");
+
+console.log('-------------');
+
+//Example 11 Array.reduce
+console.log("Example 11: Array.reduce");
+
+console.log('-------------');
+
 export default App;
